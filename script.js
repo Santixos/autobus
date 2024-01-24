5052037f9551509dfc50ffab0722a7db55cd4dc1
Santino Sottile <santinosottile80@gmail.com>
2024-01-24 18:50:21 +0100

    autobus ok

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ function executeMe() {
 	
   waitforAutobus().then (
     function(busNumber){
-        console.log('the bus that is coming is the number ' + busNumber)
-    }
+        console.log('the bus that is coming is the number ' + busNumber);
+        if(busNumber === 5) {
+          takeAPlace(busNumber);
+        } else{
+          console.log("this bus isn't for me! I need to wait for the next!");
+          executeMe();
+        }
+        
+      }
+
   ); //producing code
   // when autobus is arrived, you can get its number (see the WakeUp exercise: where "I slept x seconds" is returned)
-  let autobusNumber = undefined;
+  
   // take a seat in autobus
-  takeAPlace(autobusNumber);
+  
 }
 
 
-function takeAPlace(number) {
-	console.log('You are riding with the autobus number ' + number + '!');
+function takeAPlace() {
+	console.log('You are riding with the autobus number 5!');
 }
 
 function waitforAutobus() {
-	
+	console.log("I'm waiting for my bus!");
   let myPromise = new Promise(function(resolve){ // gli argomenti in questo caso sono funzioni (di cui una sola delle due e' eseguita), la prima è una funzione che se eseguita significa che la richiestae' andata a buon fine
     // random delay in milliseconds
 	const delay = Math.floor(Math.random() * 10000);
@@ -25,7 +33,6 @@ function waitforAutobus() {
         () => {
             // random autobus number
             const autobusNumber = Math.floor(Math.random() * 10 + 1);
-            console.log('autobus number ' + autobusNumber + ' came!');
             resolve(autobusNumber);
             
         },
